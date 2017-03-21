81700a0ea3e0838eb7f1d878780cf82946806042
root <root@localhost.localdomain>
2017-03-21 12:18:08 +0100

    Committer: root <root@localhost.localdomain>

--- lokalne_repo.py
+++ lokalne_repo.py
@@ -9,14 +9,15 @@ parser.add_argument('-t', action='store_true' , help='Tworzenie lokalnego repozy
 parser.add_argument('-k', action='store_true' , help='Kasowanie lokalnego repozytorium')
 args= parser.parse_args()
 
-if args.t == True:
 	# Tworzenie lokalnego repozytorium
+if args.t == True:
 	print ('\x1b[1;34m' + 'TWORZENIE lokalnego repozytorium' + '\x1b[0m')
 	os.system("mkdir /mnt/repo")
 	os.system("mount /dev/sdb1 /mnt/repo")
-	os.system("ln -s mnt/repo/repo/debian /var/www/debian")
-	os.system("ln -s mnt/repo/repo/kali /var/www/kali")
-	os.system("echo 'deb http://127.0.0.1/debian stable main contrib non-free' > /etc/apt/sources.list")
+	os.system("ln -s /mnt/repo/repo/debian/mirror/ftp.us.debian.org/debian /var/www/html/debian")
+	os.system("ln -s /mnt/repo/repo/kali/mirror/http.kali.org/kali /var/www/html/kali")
+	os.system("cp /etc/apt/sources.list /etc/apt/sources.list.bak")
+	os.system("echo 'deb http://127.0.0.1/debian unstable main contrib non-free' > /etc/apt/sources.list")
 	os.system("echo 'deb http://127.0.0.1/kali kali-rolling main contrib non-free' >> /etc/apt/sources.list")
 	os.system("service apache2 restart")
 	print('\x1b[1;32m' + 'ZROBIONE' + '\x1b[0m')
@@ -25,12 +26,12 @@ if args.t == True:
 	cd /var/spool/apt-mirror/mirror/ftp.de.debian.org/debian/dists
 	ln -s sarge/ stable
 	'''
-elif args.k == True:
 	# Kasowanie lokalnego repozytorium
+elif args.k == True:
 	print ('\x1b[1;34m' + 'KASOWANIE lokalnego repozytorium' + '\x1b[0m')
-	os.system("echo 'deb http://ftp.pl.debian.org/debian jessie main contrib non-free' > /etc/apt/sources.list")
-	os.system("rm -f /var/www/debian")
-	os.system("rm -f /var/www/kali")
+	os.system("cp /etc/apt/sources.list.bak /etc/apt/sources.list")
+	os.system("rm -f /var/www/html/debian")
+	os.system("rm -f /var/www/html/kali")
 	os.system("umount /mnt/repo")
 	os.system("rm -rf /mnt/repo")
 	os.system("service apache2 restart")
