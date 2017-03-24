6b63bdbe175674f1fb642c3e3db278116aaab94a
i0bax <i0bax@protonmail.com>
2017-03-24 10:16:22 +0100

    zmiana repozytorium debiana2

--- lokalne_repo.py
+++ lokalne_repo.py
@@ -14,7 +14,7 @@ if args.t == True:
 	print ('\x1b[1;34m' + 'TWORZENIE lokalnego repozytorium' + '\x1b[0m')
 	os.system("mkdir /mnt/repo")
 	os.system("mount /dev/sdb1 /mnt/repo")
-	os.system("ln -s /mnt/repo/repo/debian/mirror/ftp.us.debian.org/debian /var/www/html/debian")
+	os.system("ln -s /mnt/repo/repo/debian/mirror/ftp.pl.debian.org/debian /var/www/html/debian")
 	os.system("ln -s /mnt/repo/repo/kali/mirror/http.kali.org/kali /var/www/html/kali")
 	os.system("cp /etc/apt/sources.list /etc/apt/sources.list.bak")
 	os.system("echo 'deb http://127.0.0.1/debian unstable main contrib non-free' > /etc/apt/sources.list")
